1de8bc5d5c09aefc7363cf5de24c2c26412b9937
Figure0ut <162006401+Figure0ut@users.noreply.github.com>
2024-12-11 14:38:39 -0500

    Se añaden las clases
    Se añaden las clases con sus funcionalidades

---
+++ gestion/Zona.py
@@ -0,0 +1,16 @@
+from .Zoologico import Zoologico
+
+class Zona:
+    def __init__(self, nombre, zoo):
+        self.nombre = nombre
+        self.zoo = zoo
+        self.animales = []
+
+    def __init__(self):
+        pass
+    
+    def agregarAnimales(self, animal):
+        self.animales.append(animal)
+
+    def cantidadAnimales(self):
+        return len(self.animales)
---
+++ gestion/Zoologico.py
@@ -0,0 +1,19 @@
+from .Zona import Zona
+
+class Zoologico:
+    def __init__(self, nombre, ubicacion):
+        self.nombre = nombre
+        self.ubicacion = ubicacion
+        self.zonas = []
+
+    def __init__(self):
+        pass
+    
+    def agregarZona(self, zona):
+        self.zonas.append(zona)
+
+    def cantidadTotalAnimales(self):
+        totalAnimales = 0
+        for zona in self.zonas:
+            totalAnimales += zona.cantidadAnimales() 
+        return totalAnimales
